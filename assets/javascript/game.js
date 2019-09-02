e44101f11b2ad14c3d9bfd57bb3701c44bd1f795
Kerwin Hy <kerwin.hy@gmail.com>
2019-09-02 15:55:53 -0700

    create function to randomize cluster values

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,23 @@
 /// begin crystal collector js code
 
-// declaring variables
-var crys1 = $("#earth"),
-    crys2 = $("#fire"),
-    crys3 = $("#ice"),
-    crys4 = $("#lightning"),
+/// declaring variables
+var clust1 = $("#earth"),
+    clust2 = $("#fire"),
+    clust3 = $("#ice"),
+    clust4 = $("#lightning"),
     textWin = $("#wins").text(),
     textLose = $("#losses").text(),
     textClusters = $("#clusters").text(),
     textGoal = $("#goal").text();
 
-// functions
+/// functions
+// generate random values for each cluster
+function randCluster(obj1, obj2, obj3, obj4) {
+    clust1.val(Math.floor(Math.random() * 12) + 1);
+    clust2.val(Math.floor(Math.random() * 12) + 1);
+    clust3.val(Math.floor(Math.random() * 12) + 1);
+    clust4.val(Math.floor(Math.random() * 12) + 1);
+}
 
-// events
+randCluster(clust1, clust2, clust3, clust4);
+/// events
