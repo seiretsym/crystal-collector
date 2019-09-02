f764b2774a836aa88ab6bc7a0d9ba45bc7e2e537
Kerwin Hy <kerwin.hy@gmail.com>
2019-09-02 15:47:27 -0700

    jquery logic for index.html identifiers

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+/// begin crystal collector js code
+
+// declaring variables
+var crys1 = $("#earth"),
+    crys2 = $("#fire"),
+    crys3 = $("#ice"),
+    crys4 = $("#lightning"),
+    textWin = $("#wins").text(),
+    textLose = $("#losses").text(),
+    textClusters = $("#clusters").text(),
+    textGoal = $("#goal").text();
+
+// functions
+
+// events
